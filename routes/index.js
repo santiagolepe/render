216ebe758f64da246bc760b9cad49c6140ecea51
Octavio Santiago <santiagolepe@gmail.com>
2013-12-16 10:47:39 -0600

    add middleware for authentication, and routes for home site, login, logout and home

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,33 @@
+var homeController = require('../controllers/home');
+
+//MIDDLEWARE for authentication
+var auth = function(req, res, next){
+  if(!req.session.usr){
+      return res.render('index', 
+                        {error: 'No has iniciado session, logueate para continuar',
+                         user: 'invitado',
+                         nav: 'home' });
+  }
+  next();
+}; 
+
 module.exports = function(app){
 
-  app.get('/', function(req, res){
-    res.render('index');   
-  });
+  //home request
+  app.get('/', homeController.home.bind(homeController));
+  app.post('/logout', homeController.logout.bind(homeController));
+  app.post('/', homeController.login.bind(homeController));
 
   app.get('/payments', function(req, res){
     res.render('payments/index');   
+  });
+
+  app.get('/clients', auth, function(req, res){
+    res.render('clients/index', {
+      user: req.session.usr.first_name,
+      logged: true,
+      nav: 'clients'
+    });   
   });
 
 }
